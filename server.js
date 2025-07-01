198510e9bdd0fb2e348fb57545db48e4219fc655
luizmann <luizmannsale@gmail.com>
2025-07-01 22:46:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-(código do server.js como colado anteriormente, omitido aqui por espaço)
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+  res.send('App rodando corretamente!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
